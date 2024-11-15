fc60bfe24642ad66a2c6ec59d1fc64f852003ce0
Sam <samhooker@gmail.com>
2024-11-14 20:26:53 -0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="container mx-auto flex h-screen w-full justify-center sm:items-start md:items-center">
 	<div
-		class="grid-container bg-lightCream grid w-full grid-rows-1 rounded-xl sm:h-[500px] sm:grid-cols-1 sm:grid-rows-[42%_58%] sm:gap-0 md:h-[350px] md:w-[560px] md:grid-cols-[48%_52%] md:grid-rows-1"
+		class="grid-container grid w-full grid-rows-1 rounded-xl bg-white sm:h-[500px] sm:grid-cols-1 sm:grid-rows-[42%_58%] sm:gap-0 md:h-[350px] md:w-[560px] md:grid-cols-[48%_52%] md:grid-rows-1"
 	>
 		<Result />
 		<Summary />
